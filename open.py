abbaca9b8fb9f824bbc0e1f49ed839b600d4499f
4naan <j.cassidy038@gmail.com>
2022-04-19 23:23:33 +1000

    iterating dates pulled from excel

--- open.py
+++ open.py
@@ -1,12 +1,26 @@
 
-
+import datetime
 from openpyxl import load_workbook;
 #load in excel data
 workbook_active_date= load_workbook(filename=r"C:\Users\jcass\Documents\Python\WeatherMe\IDCJDW7007.202204.xlsx");
 workbook_historic= load_workbook(filename=r"C:\Users\jcass\Documents\Python\WeatherMe\IDCJCM0033_094008.xlsx");
 sheet_active_date = workbook_active_date.active;
 sheet_historic = workbook_historic.active;
-#
-input();
+
+
+year = 2022
+month = int(input('Enter a month'))
+day = int(input('Enter a day'))
+date_format = datetime.datetime(year, month, day)
+
+#this finds the dates
+active_dates=[]
+for row in sheet_active_date.iter_rows(min_row=8,max_row=26,min_col=2,max_col=2,values_only=True):
+    active_dates.append(row)
+for x in active_dates:
+    for y in x:
+        print(y)
+        if y == date_format:
+            print("success")
 
 #python C:\Users\jcass\Documents\Python\WeatherMe\open.py
