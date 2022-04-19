d0123bf6f38bb1f4bba3ac7a39cbffaf9f361e0d
4naan <j.cassidy038@gmail.com>
2022-04-19 23:43:45 +1000

    adding row finder

--- open.py
+++ open.py
@@ -7,20 +7,27 @@ workbook_historic= load_workbook(filename=r"C:\Users\jcass\Documents\Python\Weat
 sheet_active_date = workbook_active_date.active;
 sheet_historic = workbook_historic.active;
 
-
+#enter the date to compare
 year = 2022
-month = int(input('Enter a month'))
+month = 4 #int(input('Enter a month'))
 day = int(input('Enter a day'))
 date_format = datetime.datetime(year, month, day)
 
 #this finds the dates
+selected_row_num=0
+end_for_loop=False;
 active_dates=[]
 for row in sheet_active_date.iter_rows(min_row=8,max_row=26,min_col=2,max_col=2,values_only=True):
     active_dates.append(row)
+#find row in dates
 for x in active_dates:
-    for y in x:
-        print(y)
-        if y == date_format:
-            print("success")
+    if end_for_loop==False:
+        for y in x:
+            if y == date_format:
+                end_for_loop=True;
+                break
+            selected_row_num=selected_row_num+1
+
+
 
 #python C:\Users\jcass\Documents\Python\WeatherMe\open.py
