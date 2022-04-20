f212e64053301ef62160349ab4f0c1bd856437ab
4naan <94906780+4naan@users.noreply.github.com>
2022-04-20 11:23:10 +1000

    updated excel files

--- open.py
+++ open.py
@@ -2,8 +2,8 @@
 import datetime
 from openpyxl import load_workbook;
 #load in excel data
-workbook_active_date= load_workbook(filename=r"C:\Users\jcass\Documents\Python\WeatherMe\IDCJDW7007.202204.xlsx");
-workbook_historic= load_workbook(filename=r"C:\Users\jcass\Documents\Python\WeatherMe\IDCJCM0033_094008.xlsx");
+workbook_active_date= load_workbook(filename=r"IDCJDW7007.202204.xlsx");
+workbook_historic= load_workbook(filename=r"IDCJCM0033_094008.xlsx");
 sheet_active_date = workbook_active_date.active;
 sheet_historic = workbook_historic.active;
 
